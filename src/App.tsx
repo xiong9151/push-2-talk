4288766a43566dbc803f82a4c5f0dd2c0ef21095
xiong9151 <2473528180@qq.com>
2026-07-13 00:02:00 +0800

    fix: remove runAsAdmin from useAppServiceController params

--- src/App.tsx
+++ src/App.tsx
@@ -389,8 +389,6 @@ function App() {
     setError,
     enableAutostart,
     setEnableAutostart,
-    runAsAdmin,
-    setRunAsAdmin,
     enableMuteOtherApps,
     setEnableMuteOtherApps,
     theme,
